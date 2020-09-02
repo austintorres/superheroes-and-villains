feb08fffd18a66567b807fa243b7b1856504fa29
austintorres <torresgaustin@gmail.com>
2020-09-02 12:28:12 -0400

    Character card successful

--- src/App.js
+++ src/App.js
@@ -1,33 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import SearchBar from './components/SearchBar';
 import CharacterCard from './components/CharacterCard';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
+import SelectionModal from './components/SelectionModal';
 
 function App() {
+  const [characterInfo, setCharacterInfo] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [show, setShow] = useState(false);
+  const [characterList, setCharacterList] = useState(null);
 
-  const [ characterInfo, setCharacterInfo ] = useState();
-  const [ characterName, setCharacterName ] = useState('batman')
-
-  const getCharacter = async () => {
+  const getCharacter = async (characterName) => {
+    setIsLoading(true);
     try {
-      const response = await fetch(`https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/3401549706550150/search/${characterName}`);
-      const json = await response.json();
-      console.log('json', json)
-      if (!response.ok) {
-        throw Error(response.statusText);
+      const response = await fetch(
+        `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/3401549706550150/search/${characterName}`
+      );
+      const data = await response.json();
+      if (!response.ok || data.response === 'error') {
+        throw Error(`"${characterName}" not found. Try again!`);
       }
-      setCharacterInfo(json);
-    } catch (error) {
-      console.log(error);
+      const filteredData = data.results.filter(
+        (item) => characterName.toLowerCase() === item.name.toLowerCase()
+      );
+      setCharacterList(filteredData);
+      if (filteredData.length === 1) {
+        setCharacterInfo(filteredData[0]);
+      } else {
+        setShow(true);
+      }
+    } catch (err) {
+      setError(err.message);
+      setCharacterInfo(null);
     }
-  }
+    setIsLoading(false);
+  };
 
   return (
-    <Container>
-      <h1>Super-Heroes & Villains</h1>
-      <Button onClick={() => getCharacter()}>Get Character</Button>
-      <SearchBar />
-      <CharacterCard />
+    <Container fluid>
+      <Row className="justify-content-center">
+        <h1>Heroes and Villains</h1>
+      </Row>
+      <SearchBar
+        error={error}
+        isLoading={isLoading}
+        setError={setError}
+        getCharacter={getCharacter}
+      />
+      {characterInfo && !error ? <CharacterCard characterInfo={characterInfo} /> : null}
+      <SelectionModal
+        show={show}
+        setCharacterInfo={setCharacterInfo}
+        characterList={characterList}
+        setShow={setShow}
+      />
     </Container>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Row, Button, Form, Spinner } from 'react-bootstrap';
 
-function SearchBar() {
+function SearchBar({ getCharacter, error, setError, isLoading }) {
+  const [input, setInput] = useState('');
+
+  const handleChange = (e) => {
+    setError(null);
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getCharacter(input);
+    setInput('');
+  };
+
   return (
-    <div className="SearchBar">
-      <h1>Search</h1>
-    </div>
+    <Row className="justify-content-center">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Search for a hero or villain!</Form.Label>
+          <Form.Control placeholder="e.g. Superman" value={input} onChange={handleChange} />
+          {error ? <Form.Text className="text-danger">{error}</Form.Text> : null}
+        </Form.Group>
+        {isLoading ? (
+          <Button variant="primary" disabled>
+            <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />
+            Loading...
+          </Button>
+        ) : (
+          <Button type="submit">Get Character</Button>
+        )}
+      </Form>
+    </Row>
   );
 }
 
