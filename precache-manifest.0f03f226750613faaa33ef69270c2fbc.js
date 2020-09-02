789fa49fa0442db8d184f5eec3232af5cb665177
austintorres <torresgaustin@gmail.com>
2020-09-02 18:41:30 -0400

    Updates

---
+++ precache-manifest.0f03f226750613faaa33ef69270c2fbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0baae75c859f29bcb93db403803ca6f8",
+    "url": "/superheroes-and-villains/index.html"
+  },
+  {
+    "revision": "3feffd333b2a20c50a54",
+    "url": "/superheroes-and-villains/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2ba06f23604ac349acde",
+    "url": "/superheroes-and-villains/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3feffd333b2a20c50a54",
+    "url": "/superheroes-and-villains/static/js/2.c04491a8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/superheroes-and-villains/static/js/2.c04491a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba06f23604ac349acde",
+    "url": "/superheroes-and-villains/static/js/main.f20eaba8.chunk.js"
+  },
+  {
+    "revision": "0eec1dea42ea5fb6fb9c",
+    "url": "/superheroes-and-villains/static/js/runtime-main.34c7d7e9.js"
+  }
+]);
