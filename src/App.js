0b76006a5b4a30df13cad05c86feaab227b69b50
austintorres <torresgaustin@gmail.com>
2020-09-01 22:18:48 -0400

    Added api + functions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './components/SearchBar';
 import CharacterCard from './components/CharacterCard';
+import { Container, Button } from 'react-bootstrap';
 
 function App() {
+
+  const [ characterInfo, setCharacterInfo ] = useState();
+  const [ characterName, setCharacterName ] = useState('batman')
+
+  const getCharacter = async () => {
+    try {
+      const response = await fetch(`https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/3401549706550150/search/${characterName}`);
+      const json = await response.json();
+      console.log('json', json)
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      setCharacterInfo(json);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div className="App">
+    <Container>
       <h1>Super-Heroes & Villains</h1>
+      <Button onClick={() => getCharacter()}>Get Character</Button>
       <SearchBar />
       <CharacterCard />
-    </div>
+    </Container>
   );
 }
 
